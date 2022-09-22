f28d56a66d2eea0c763dac4bf8c3c1a862ce1ec9
Marcos <marsanben92@gmail.com>
2022-09-23 01:32:34 +0200

    feat: map maps

---
+++ maps/maps.go
@@ -0,0 +1,72 @@
+package maps
+
+import (
+	"github.com/sonirico/stadio/fp"
+	"github.com/sonirico/stadio/tuples"
+)
+
+func Equals[K comparable, V any](m1, m2 map[K]V, eq func(V, V) bool) bool {
+	if len(m1) != len(m2) {
+		return false
+	}
+
+	if m1 == nil && m2 != nil {
+		return false
+	}
+
+	if m1 != nil && m2 == nil {
+		return false
+	}
+
+	for k1, v1 := range m1 {
+		v2, ok := m2[k1]
+		if !ok {
+			return false
+		}
+
+		if !eq(v1, v2) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func Map[K1 comparable, V1 any, K2 comparable, V2 any](
+	m map[K1]V1,
+	p func(K1, V1) (K2, V2),
+) map[K2]V2 {
+	if m == nil {
+		return nil
+	}
+
+	res := make(map[K2]V2, len(m))
+
+	for k1, v1 := range m {
+		k2, v2 := p(k1, v1)
+		res[k2] = v2
+	}
+
+	return res
+}
+
+func FilterMap[K1 comparable, V1 any, K2 comparable, V2 any](
+	m map[K1]V1,
+	p func(K1, V1) fp.Option[tuples.Tuple2[K2, V2]],
+) map[K2]V2 {
+	if m == nil {
+		return nil
+	}
+
+	res := make(map[K2]V2, len(m))
+
+	for k1, v1 := range m {
+		tpl := p(k1, v1)
+		if tpl.IsSome() {
+			v := tpl.UnwrapUnsafe()
+			res[v.V1] = v.V2
+		}
+	}
+
+	return res
+}
---
+++ tuples/tuples.go
@@ -0,0 +1,8 @@
+package tuples
+
+type (
+	Tuple2[T1, T2 any] struct {
+		V1 T1
+		V2 T2
+	}
+)
